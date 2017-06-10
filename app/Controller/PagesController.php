368f1614ce39ef9b60b914d458d81fe2afe2aac3
ravichandran1383 <contact@bigspire.com>
2017-06-10 15:47:52 +0530

    new changing

--- app/Controller/PagesController.php
+++ app/Controller/PagesController.php
@@ -16,6 +16,8 @@
  * @package       app.Controller
  * @since         CakePHP(tm) v 0.2.9
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ 
+ for tsting
  */
 
 App::uses('AppController', 'Controller');
