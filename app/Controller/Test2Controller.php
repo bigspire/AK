e09f64453bd7440a125aa7a318370ecde9ebe5d2
ravichandran1383 <contact@bigspire.com>
2017-06-11 18:04:36 +0530

    Create Test2Controller.php

--- app/Controller/Test2Controller.php
+++ app/Controller/Test2Controller.php
@@ -4,7 +4,7 @@
  *
  * This file will render views from views/pages/
  *
- * PHP 5
+ * PHP 5 - test
  *
  * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
  * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
